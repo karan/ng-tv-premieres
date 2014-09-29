4e59e9dc32e3c35d6108919bf985daa3133c6e0c
Karan Goel <karan@goel.im>
2014-09-28 21:48:30 -0700

    api call works

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,8 +1,18 @@
 app.controller("mainController", function($scope, $http){
 
-    $scope.init = function() {
-
-    };
+  $scope.apiKey = '82226981b8c69c86a06f7740a3c14efb';
+  $scope.init = function() {
+    var today = new Date();
+    var apiDate = today.getFullYear() +
+                  ('0' + (today.getMonth() + 1)).slice(-2) + '' +
+                  ('0' + today.getDate()).slice(-2);
+    $http.jsonp('http://api.trakt.tv/calendar/premieres.json/' +
+                $scope.apiKey + '/' + apiDate + '/' + 10 +
+                '?callback=JSON_CALLBACK').success(function(data) {
+                  console.log(data);
+                }).error(function(err) {
+                  console.log(err);
+                 });
+  };
 
 });
-
